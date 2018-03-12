872a7dd8057b7c954b6faf822271804a71d6845c
astrax <ammarahmed.ph@gmail.com>
2018-03-12 23:10:47 +0100

    save sites info in a dictionary

---
+++ AWESOME/NarrowBand/plot_narrowband/SitesInfo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Author: Ahmed Ammar, ahmed.ammar@fst.utm.tn
+Purpose: Savde VLF Transmitters and Receivers Info
+Date Created: Mon Mar 12 22:23:34 2018
+   
+'''
+# TODO: Add All Rx Sites
+Rx_sites = {
+            "Algeria":"AL",
+            "Tunisia":"TN"
+            }
+
+# TODO: Create dictionary for Tx Sites
