31a460e99543d004a0ae61c0c386872596325588
astrax <ammarahmed.ph@gmail.com>
2018-03-27 13:09:03 +0100

    Copy one month of recorded data

---
+++ AWESOME/NarrowBand/concatinate_data/CopyData.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+Author: Ahmed Ammar, ahmed.ammar@fst.utm.tn
+Purpose: - - - 
+Inputs: - - -
+Outputs:  - - -
+Date Created: Tue Mar 27 11:48:12 2018
+Date Modified: M D, Y
+Date Released: M D, Y
+Versions:
+    V0.01: ---
+    
+'''
+import glob
+import shutil
+for ii in range(1,31):
+    dd= str('{:02d}'.format(ii))
+    
+    FileList=glob.glob("H:/NarrowbandData/Tunisia/2017/09/"+ dd+ "/*NRK*A.mat")
+    dest_dir="H:/NarrowbandData/Tunisia/2017/09/NRK"
+    for filename in FileList:
+        print(filename)
+        shutil.copy(filename, dest_dir)
